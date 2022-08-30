e4ca5646088f8d08406240808f6756f1b600a407
gordonpm <gordonm0815@gmail.com>
2022-08-29 20:44:11 -0700

    Added fallback method for the api method Added circuit breaker through resilience4j

--- src/main/java/io/gordonpm/bicyclecatalogservice/controller/BicycleCatalogController.java
+++ src/main/java/io/gordonpm/bicyclecatalogservice/controller/BicycleCatalogController.java
@@ -1,5 +1,6 @@
 package io.gordonpm.bicyclecatalogservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.gordonpm.bicyclecatalogservice.models.Bicycle;
 import io.gordonpm.bicyclecatalogservice.models.CatalogItem;
 import io.gordonpm.bicyclecatalogservice.models.UserRating;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,7 +21,10 @@ public class BicycleCatalogController {
     @Autowired
     private RestTemplate restTemplate;
 
+    public static final String CATALOG_SERVICE = "catalogService";
+
     @RequestMapping("/{userId}")
+    @CircuitBreaker(name = CATALOG_SERVICE, fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         UserRating ratings = restTemplate.getForObject("http://bicycle-ratings-service/bicycleratings/users/" + userId, UserRating.class);
@@ -32,5 +37,9 @@ public class BicycleCatalogController {
                 return null;
             }
         }).collect(Collectors.toList());
+    }
+
+    public List<CatalogItem> getFallbackCatalog(Exception e) {
+        return Arrays.asList(new CatalogItem("No Bicycle", "", 0));
     }
 }
